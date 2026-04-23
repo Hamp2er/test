504e383e44a23b88d56c615c971b380c263619e0
Hampthor <hampthor@MacBook-Air-Hampthor.local>
2026-04-23 10:01:22 +0500

    latest

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from std_msgs.msg import Float64
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist # Наш главный герой для движения
@@ -7,6 +8,7 @@ class FigureMover(Node):
         super().__init__('motorpins')
         self.create.publisher_()
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.subscription = self.create_subscription(Float64,"/range",self.range_callback, 10)
         self.timer_for1()
         self.declare_parameter("left_motor_pin",0)
         self.declare_parameter("right_motor_pin",1)
@@ -31,6 +33,10 @@ class FigureMover(Node):
             stop.msg = Twist()
             self.publisher_.publish(stop_msg)
             self.timer.cancel()
+    def range_callback(self):
+        if msg.data < 0.5:
+            self.get_logger().warn('wall sts close')
+            self.lifetime = 0 
 
 
     
